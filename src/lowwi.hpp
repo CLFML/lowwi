579e4ac9e16f9f5422ab774c4215e8cfe35f980c
Victor Hogeweij <hogeweyv@gmail.com>
2024-12-30 22:18:21 +0100

    forgot to document the structs

--- src/lowwi.hpp
+++ src/lowwi.hpp
@@ -30,11 +30,34 @@
 
 namespace CLFML::LOWWI
 {
-    typedef struct {
+    /**
+     * @brief Lowwi context which gets passed in to the callback
+     * @param phrase The model identifier of the model that was triggered
+     * @param confidence The model confidence; 
+     *                   How certain was the model on the wakeword presence in audio?
+     */
+    typedef struct
+    {
         const std::string phrase;
         const float confidence;
-    }Lowwi_ctx_t;
+    } Lowwi_ctx_t;
 
+    /**
+     * @brief The user-provided struct which provides the classifier model settings
+     * @param phrase The model identifier which get's passed into the callback function when triggered.
+     * @param model_path The classifier model path
+     * @param cbfunc Function pointer to a callback function that get's called when wakeword is triggered
+     * @param cb_arg Additional function argument that get's passed in the callback when wakeword is triggered
+     *               (void pointer)
+     * @param refractory The negative feedback on activation, when activated this factor makes the debouncing work :)
+     *                   Increasing it gives a higher negative bounty, thus dampening any further activations.
+     *                   (Default = 20)
+     *
+     * @param threshold The threshold determines whether model confidence is worth acting on (default = 0.5f)
+     * @param min_activations Number of activations the model should have to be considered detected
+     *                       (Default = 5, but depends on how well the model is trained and how easy to detect)
+     *                       (It's like a debouncing system)
+     */
     struct Lowwi_word_t
     {
         std::string phrase = "";
@@ -45,8 +68,7 @@ namespace CLFML::LOWWI
         float threshold = 0.5f;
         float min_activations = 5;
         uint8_t debug = false;
-
-    } ;
+    };
 
     class Lowwi
     {
@@ -54,11 +76,11 @@ namespace CLFML::LOWWI
         Lowwi();
 
         /**
-        * @brief Add new wakeword to detection runtime
-        * @param lowwi_word Struct with the properties 
-        *                   of the to be added wakeword
-        */
-        void add_wakeword(const Lowwi_word_t& lowwi_word);
+         * @brief Add new wakeword to detection runtime
+         * @param lowwi_word Struct with the properties
+         *                   of the to be added wakeword
+         */
+        void add_wakeword(const Lowwi_word_t &lowwi_word);
 
         /**
          * @brief Remove wakeword from detection runtime
@@ -88,8 +110,9 @@ namespace CLFML::LOWWI
          */
         std::vector<float> _mel_samples;
         std::vector<float> _feature_samples;
-        
-        typedef struct {
+
+        typedef struct
+        {
             std::unique_ptr<WakeWord> ww_inst;
             Lowwi_word_t properties;
         } wakeword_t;
