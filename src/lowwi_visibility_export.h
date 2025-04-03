f5a827694bebfe007916bed5618d47360651bae0
Hoog-v <hogeweyv@gmail.com>
2025-04-03 15:01:40 +0200

    -it should now work

--- src/lowwi_visibility_export.h
+++ src/lowwi_visibility_export.h
@@ -15,15 +15,11 @@
 #else
 #define LOWWI_PUBLIC LOWWI_IMPORT
 #endif
-#endif
-#else
-#define LOWWI_EXPORT __attribute__((visibility("default")))
-#define LOWWI_IMPORT
-#if __GNUC__ >= 4
-#define LOWWI_PUBLIC __attribute__((visibility("default")))
 #else
 #define LOWWI_PUBLIC
 #endif
+#else
+#define LOWWI_PUBLIC
 #endif
 
 #endif // LOWWI__VISIBILITY_CONTROL_H_
