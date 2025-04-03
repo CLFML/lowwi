da0d027ec9e36c9fd867adb7d40f4ccb003eec9b
Hoog-v <hogeweyv@gmail.com>
2025-04-03 14:54:40 +0200

    lint the lowwi_visibility thing

--- src/lowwi_visibility_export.h
+++ src/lowwi_visibility_export.h
@@ -2,26 +2,26 @@
 #define LOWWI__VISIBILITY_CONTROL_H_
 
 #if defined _WIN32 || defined __CYGWIN__
-  #ifdef __GNUC__
-    #define LOWWI_EXPORT __attribute__ ((dllexport))
-    #define LOWWI_IMPORT __attribute__ ((dllimport))
-  #else
-    #define LOWWI_EXPORT __declspec(dllexport)
-    #define LOWWI_IMPORT __declspec(dllimport)
-  #endif
-  #ifdef LOWWI_BUILDING_DLL
-    #define LOWWI_PUBLIC LOWWI_EXPORT
-  #else
-    #define LOWWI_PUBLIC LOWWI_IMPORT
-  #endif
+#ifdef __GNUC__
+#define LOWWI_EXPORT __attribute__((dllexport))
+#define LOWWI_IMPORT __attribute__((dllimport))
 #else
-  #define LOWWI_EXPORT __attribute__ ((visibility ("default")))
-  #define LOWWI_IMPORT
-  #if __GNUC__ >= 4
-    #define LOWWI_PUBLIC __attribute__ ((visibility ("default")))
-  #else
-    #define LOWWI_PUBLIC
-  #endif
+#define LOWWI_EXPORT __declspec(dllexport)
+#define LOWWI_IMPORT __declspec(dllimport)
+#endif
+#ifdef LOWWI_BUILDING_DLL
+#define LOWWI_PUBLIC LOWWI_EXPORT
+#else
+#define LOWWI_PUBLIC LOWWI_IMPORT
+#endif
+#else
+#define LOWWI_EXPORT __attribute__((visibility("default")))
+#define LOWWI_IMPORT
+#if __GNUC__ >= 4
+#define LOWWI_PUBLIC __attribute__((visibility("default")))
+#else
+#define LOWWI_PUBLIC
+#endif
 #endif
 
-#endif  // LOWWI__VISIBILITY_CONTROL_H_
+#endif // LOWWI__VISIBILITY_CONTROL_H_
