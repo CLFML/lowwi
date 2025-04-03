5c0bde3fa64e8d9122699a27800668c0521ec476
Hoog-v <hogeweyv@gmail.com>
2025-04-03 14:47:06 +0200

    add back activation scripts

--- rattler/bld_ament_cmake.bat
+++ rattler/bld_ament_cmake.bat
@@ -32,3 +32,10 @@ if errorlevel 1 exit 1
 :: see the comment before setting the CMAKE_GENERATOR env variable
 cmake --build . --config Release --parallel %CPU_COUNT% --target install
 if errorlevel 1 exit 1
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+)
